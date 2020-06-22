e8ccab7939ded4298c2b0f3cbc68f75066198a49
xuhuaren <xuhuaren@tencent.com>
2020-06-22 14:02:11 +0800

    update

--- gen.py
+++ gen.py
@@ -16,7 +16,7 @@ Entry-point for generating synthetic text images, as described in:
 import numpy as np
 import h5py
 import os, sys, traceback
-os.environ["OMP_NUM_THREADS"] = "1"
+#os.environ["OMP_NUM_THREADS"] = "2"
 import os.path as osp
 from synthgen import *
 from common import *
@@ -30,7 +30,7 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 ## Define some configuration variables:
 NUM_IMG = -1 # no. of images to use for generation (-1 to use all available):
-INSTANCE_PER_IMAGE = 1 # no. of times to use the same image
+INSTANCE_PER_IMAGE = 100 # no. of times to use the same image
 SECS_PER_IMG = 5 #max time per image in seconds
 
 # path to the data-file, containing image, depth and segmentation:
@@ -38,7 +38,8 @@ DATA_PATH = 'data'
 DB_FNAME = osp.join(DATA_PATH,'dset_8000.h5')
 # url of the data (google-drive public file):
 DATA_URL = 'http://www.robots.ox.ac.uk/~ankush/data.tar.gz'
-OUT_FILE = 'results/SynthText_8k.h5'
+OUT_FILE = 'results/SynthText_8k_parallel.h5'
+executor = futures.ThreadPoolExecutor(max_workers=10)
 
 def get_data():
   """
@@ -112,6 +113,69 @@ def rgb2gray(image):
 
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
     return gray
+
+
+
+def single_line(temps):
+    i = temps
+    os.environ["OMP_NUM_THREADS"] = "1"
+    imname = imnames[i]
+    try:
+      # get the image:
+      img = Image.fromarray(db['image'][imname][:])
+      depth = db['depth'][imname][:].T
+      depth = depth[:,:,1]
+      # get segmentation:
+      seg = db['seg'][imname][:].astype('float32')
+      area = db['seg'][imname].attrs['area']
+      label = db['seg'][imname].attrs['label']
+
+      # re-size uniformly:
+      sz = depth.shape[:2][::-1]
+      img = np.array(img.resize(sz,Image.ANTIALIAS))
+      seg = np.array(Image.fromarray(seg).resize(sz,Image.NEAREST))
+
+      print colorize(Color.RED,'%d of %d'%(i,end_idx-1), bold=True)
+      res = RV3.render_text(img,depth,seg,area,label,
+                            ninstance=INSTANCE_PER_IMAGE,viz=viz)
+      if len(res) > 0:
+        # non-empty : successful in placing text:
+        add_res_to_db(imname,res,out_db)
+      # visualize the output:
+      if viz:
+        if 'q' in raw_input(colorize(Color.RED,'continue? (enter to continue, q to exit): ',True)):
+          break
+    except:
+      traceback.print_exc()
+      print colorize(Color.GREEN,'>>>> CONTINUING....', bold=True)
+      continue 
+  
+
+def main_para(viz=False):
+  # open databases:
+  print colorize(Color.BLUE,'getting data..',bold=True)
+  global viz = viz
+  global db = get_data()
+  print colorize(Color.BLUE,'\t-> done',bold=True)
+
+  # open the output h5 file:
+  out_db = h5py.File(OUT_FILE,'w')
+  out_db.create_group('/data')
+  print colorize(Color.GREEN,'Storing the output in: '+OUT_FILE, bold=True)
+
+  # get the names of the image files in the dataset:
+  global imnames = sorted(db['image'].keys())
+  N = len(imnames)
+  global NUM_IMG
+  if NUM_IMG < 0:
+    NUM_IMG = N
+  start_idx,end_idx = 0,min(NUM_IMG, N)
+  temps = [i for i in range(start_idx,end_idx)]
+  global RV3 = RendererV3(DATA_PATH,max_time=SECS_PER_IMG)  
+  executor.map(single_line, temps)
+  db.close()
+  out_db.close()   
+  
 
 def main(viz=False):
   # open databases:
@@ -285,4 +349,4 @@ if __name__=='__main__':
   parser = argparse.ArgumentParser(description='Genereate Synthetic Scene-Text Images')
   parser.add_argument('--viz',action='store_true',dest='viz',default=False,help='flag for turning on visualizations')
   args = parser.parse_args()
-  main(args.viz)
+  main_para(args.viz)
